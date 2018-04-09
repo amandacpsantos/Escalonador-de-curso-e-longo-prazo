0c6ba60d7cf2ddffef55e84e2c88f0a656767080
amandacpsantos <contatoacps@gmail.com>
2018-04-09 10:07:10 -0300

    Correção I/O

--- File.py
+++ File.py
@@ -1,74 +1,85 @@
 class File(object):
 
-    def __init__(self, nameFileIn=None):
+    def __init__(self, nameFileIn="in.txt"):
         self.__nameFileIn = str(nameFileIn)
         self.__nameFileOut = "out.txt"
         self.__pointerFile = self.__searchPointerFile()
 
     def getMultProcess(self):
         ref_file = self.__openFile(self.__nameFileIn, 'r')
-        number = ref_file.readline().split('=')[1]
-        self.__closeFile(ref_file)
-        return number
+
+        if ref_file != None:
+            number = ref_file.readline().split('=')[1]
+            self.__closeFile(ref_file)
+            return number
+
+        return None
 
     def getProcess(self):
         listProcess = []
         ref_file = self.__openFile(self.__nameFileIn, 'r')
 
-        #ABRIR ARQUIVO E IR DIRETO AO PONTO QUE COMEÇAM OS PROCEDIMENTOS
-        ref_file.seek(self.__pointerFile[1])
+        if ref_file != None:
 
-        #PEGAR TODAS AS LINHAS REFERENTES AOS PROCEDIMENTOS E SALVAR EM UMA LISTA
-        line = ref_file.readline()
-        while line.__len__() != 2:
-            listProcess.append(line)
+            #ABRIR ARQUIVO E IR DIRETO AO PONTO QUE COMEÇAM OS PROCEDIMENTOS
+            ref_file.seek(self.__pointerFile[1])
+
+            #PEGAR TODAS AS LINHAS REFERENTES AOS PROCEDIMENTOS E SALVAR EM UMA LISTA
             line = ref_file.readline()
+            while line.__len__() != 2:
+                listProcess.append(line)
+                line = ref_file.readline()
 
-        #LIMPAR AS LINHAS REFERENTES AOS PROCEDIMENTOS SALVOS E SALVAR APENAS O VALORES EM UMA LISTA
-        listDataProcess = []
-        for processo in listProcess:
-            byEqual = processo.split('=')[1]
-            byComma = byEqual.split(",")
+            #LIMPAR AS LINHAS REFERENTES AOS PROCEDIMENTOS SALVOS E SALVAR APENAS O VALORES EM UMA LISTA
+            listDataProcess = []
+            for processo in listProcess:
+                byEqual = processo.split('=')[1]
+                byComma = byEqual.split(",")
 
-            listProcessUnit = []
-            for valor in byComma:
-                listProcessUnit.append(valor.split("-")[0])
-                if ";" in valor:
-                    listProcessUnit.append(valor[-2])
+                listProcessUnit = []
+                for valor in byComma:
+                    listProcessUnit.append(valor.split("-")[0])
+                    if ";" in valor:
+                        listProcessUnit.append(valor[-2])
 
-            listDataProcess.append(listProcessUnit)
+                listDataProcess.append(listProcessUnit)
 
-        self.__closeFile(ref_file)
-        return listDataProcess
+            self.__closeFile(ref_file)
+            return listDataProcess
+
+        return None
 
     def getOperation(self):
         listOperation = []
         ref_file = self.__openFile(self.__nameFileIn, 'r')
 
-        #ABRIR ARQUIVO E IR DIRETO AO PONTO QUE COMEÇA AS OPERAÇÕES
-        ref_file.seek(self.__pointerFile[2])
+        if ref_file != None:
+            #ABRIR ARQUIVO E IR DIRETO AO PONTO QUE COMEÇA AS OPERAÇÕES
+            ref_file.seek(self.__pointerFile[2])
 
-        #PEGAR TODAS AS LINHAS REFERENTES AS OPERAÇÕES E SALVAR EM UMA LISTA
-        line = ref_file.readline()
-        while line:
-            listOperation.append(line)
+            #PEGAR TODAS AS LINHAS REFERENTES AS OPERAÇÕES E SALVAR EM UMA LISTA
             line = ref_file.readline()
+            while line:
+                listOperation.append(line)
+                line = ref_file.readline()
 
-        #LIMPAR AS LINHAS REFERENTES AS OPERAÇÕES SALVAS E SALVAR APENAS O VALORES EM UMA LISTA
-        listDataOperation = []
-        for operacao in listOperation:
-            limp1 = operacao.split("=")[1]
-            listDataOperation.append(limp1[0])
+            #LIMPAR AS LINHAS REFERENTES AS OPERAÇÕES SALVAS E SALVAR APENAS O VALORES EM UMA LISTA
+            listDataOperation = []
+            for operacao in listOperation:
+                limp1 = operacao.split("=")[1]
+                listDataOperation.append(limp1[0])
 
-        self.__closeFile(ref_file)
-        return listDataOperation
+            self.__closeFile(ref_file)
+            return listDataOperation
+
+        return None
 
     def createOut(self, nameFileOut=None, listDada=[]):
-
         if nameFileOut == None:
             nameFileOut = self.__nameFileOut
 
         ref_file = open(nameFileOut, 'w+')
+
         ref_file.write("FCFS\n"
                        "-TE-P1=\n"
                        "-TE-P2=\n"
@@ -104,26 +115,30 @@ class File(object):
 
         ref_file = self.__openFile(self.__nameFileIn, 'r')
 
-        pointB = ref_file.tell()
-        line = ref_file.readline()
-        pointA = ref_file.tell()
+        if ref_file != None:
 
-        while line:
-            if "Multiprogramacao" in line:
-                listPointer.append(pointB);
-
-            elif "Processos" in line:
-                pointB = ref_file.tell()
-                listPointer.append(pointB)
-
-            elif "Operações" in line:
-                pointB = ref_file.tell()
-                listPointer.append(pointB)
-
-            pointB = pointA
+            pointB = ref_file.tell()
             line = ref_file.readline()
             pointA = ref_file.tell()
 
-        self.__closeFile(ref_file)
+            while line:
+                if "Multiprogramacao" in line:
+                    listPointer.append(pointB);
 
-        return listPointer
+                elif "Processos" in line:
+                    pointB = ref_file.tell()
+                    listPointer.append(pointB)
+
+                elif "Operações" in line:
+                    pointB = ref_file.tell()
+                    listPointer.append(pointB)
+
+                pointB = pointA
+                line = ref_file.readline()
+                pointA = ref_file.tell()
+
+            self.__closeFile(ref_file)
+
+            return listPointer
+
+        return None
--- Teste.py
+++ Teste.py
@@ -1,6 +1,11 @@
 from File import File as f
 
-arquivo = f("in.txt")
+#nameFileIn ou nameFileOut pode ou não ser passado.
+#Caso não há, o nome padrão é in.txt e out.txt respectivamente.
+
+nameFileIn = "in2.txt"
+
+arquivo = f(nameFileIn)
 print(arquivo.getMultProcess())
 print(arquivo.getProcess())
 print(arquivo.getOperation())
